f54304cee090b864250279b78ea17e2e634dcc04
t3sst3sst3ss <tmotherway@gmail.com>
2023-01-04 14:05:57 +0100

    scripts update

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 console.log("Hello world")
 var escapedText1 = "He said: \"Yes!\", that's for sure"
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
