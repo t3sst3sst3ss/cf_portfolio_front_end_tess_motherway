e5b149c7795953508a58a4082a4d8ebe72dd7e95
t3sst3sst3ss <tmotherway@gmail.com>
2023-01-18 10:47:13 +0100

    stylelint

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Hello world")
-var escapedText1 = "He said: \"Yes!\", that's for sure"
+console.log("Hello world");
+var escapedText1 = 'He said: "Yes!", that\'s for sure';
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
   }
+}
